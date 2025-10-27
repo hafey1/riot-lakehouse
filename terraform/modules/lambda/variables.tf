dc4d58f8b4179fb3d71bd23a52aa6e6106d8815d
Robert Niggebrugge <38027049+hafey1@users.noreply.github.com>
2025-10-27 23:43:00 +0000

    tf fmt

--- terraform/modules/lambda/variables.tf
+++ terraform/modules/lambda/variables.tf
@@ -1,15 +1,15 @@
 variable "function_name" { type = string }
-variable "region"        { type = string }
-variable "role_name"     { type = string }
-variable "zip_path"      { type = string } # e.g. ../lambda_ingest/lambda.zip
-variable "handler"       { type = string } # e.g. "lambda_ingest.app.handler"
-variable "runtime"       { type = string } # e.g. "python3.11"
+variable "region" { type = string }
+variable "role_name" { type = string }
+variable "zip_path" { type = string } # e.g. ../lambda_ingest/lambda.zip
+variable "handler" { type = string }  # e.g. "lambda_ingest.app.handler"
+variable "runtime" { type = string }  # e.g. "python3.11"
 
 variable "env_vars" {
-  type = map(string)     # multi-line form needed
+  type = map(string) # multi-line form needed
 }
 
-variable "tags"          { type = map(string) }
+variable "tags" { type = map(string) }
 
 variable "create_schedule" {
   type    = bool
